52344a797423ec82b5b7a4ac0405ea46d8931c05
L0j1k <L0j1k@L0j1k.com>
2014-03-17 02:47:42 -0500

    built out server functionality

--- tcpdev/src/tcpserver.c
+++ tcpdev/src/tcpserver.c
@@ -9,10 +9,20 @@
  * @version 0.0.1-prealpha
  */
 
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+
+#ifndef _TCPDEV_MAXCONN
+#define _TCPDEV_MAXCONN 5
+#endif
+
+void error( char *message ) {
+  printf("error: [%s]\n", message);
+  exit(1);
+}
 
 int main( int argc, char **argv ) {
   typedef enum Boolean { false, true } Boolean;
@@ -20,10 +30,22 @@ int main( int argc, char **argv ) {
   static const char *version = "0.0.1";
   static const char *phase = "prealpha";
 
+  Boolean finished = false;
   struct sockaddr_in server;
   struct hostent *h;
-  int srvsock;
+  int listensock, serversock;
   char buffer[1024];
+
+  printf("tcpserver -- TCP-based protocol development harness (server) v%s-%s\n", version, phase);
+  printf("Written by L0j1k@L0j1k.com -- Released under BSD3\n\n");
+
+  if ((serversock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) error("cannot create socket()");
+  if ((listensock = listen(serversock, _TCPDEV_MAXCONN)) < 0) error("cannot listen()");
+
+  while (!finished) {
+    // @debug begin
+    // @debug end
+  }
 
   return 0;
 }
